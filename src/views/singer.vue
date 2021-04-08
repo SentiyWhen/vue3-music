6f3788d3016d957eb84ef17d5eb163724ff2bb46
zhangww <18622076193@163.com>
2021-04-08 17:16:15 +0800

    feat: 歌手列表数据获取

--- src/views/singer.vue
+++ src/views/singer.vue
@@ -5,9 +5,21 @@
 </template>
 
 <script>
-export default {
-  name: 'singer'
-}
+  import { getSingerList } from '@/service/singer'
+
+  export default {
+    name: 'singer',
+    data() {
+      return {
+        singers: []
+      }
+    },
+    async created() {
+      const result = await getSingerList()
+      console.log(result)
+      this.singers = result.singers
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
